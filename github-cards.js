95258d993d1e77dfc4d4fe0074f22a1bec75f9a5
Kasthuri <kasthuri.14@cse.mrt.ac.lk>
2019-08-06 15:42:47 +0530

    multiple user data

--- github-cards.js
+++ github-cards.js
@@ -1,29 +1,45 @@
+const testData = [
+  {name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
+  {name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
+  {name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
+];
+
+
+const CardList = (props) => (
+<div>
+<Card {...testData[0]} />
+<Card {...testData[1]} />
+</div>
+);
+
 class Card extends React.Component {
-  render() {
-    return (
-      <div className="github-profile">
-        <img src="https://placehold.it/75" />
-        <div className="info">
-          <div className="name">Name here...</div>
-          <div className="company">Company here...</div>
-        </div>
-      </div>
-    );
-  }
+render() {
+const data = this.props;
+console.log(data);
+return (
+  <div className="github-profile">
+    <img src={data.avatar_url} />
+    <div className="info">
+      <div className="name">{data.name}</div>
+      <div className="company">{data.company}</div>
+    </div>
+  </div>
+);
+}
 }
 
 class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <div className="header">{this.props.title}</div>
-        <Card />
-      </div>
-    );
-  }
+render() {
+return (
+  <div>
+    <div className="header">{this.props.title}</div>
+    <CardList />
+  </div>
+);
+}
 }
 
 ReactDOM.render(
-	<App title="The GitHub Cards App" />,
-  mountNode,
+<App title="The GitHub Cards App" />,
+mountNode,
 );
